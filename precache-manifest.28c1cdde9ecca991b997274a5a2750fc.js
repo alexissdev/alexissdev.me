eb31a2fe0505fa38f53619dfd1282714c5d2fb08
alexissdev <notcacha1998@gmail.com>
2023-02-14 15:58:09 -0300

    Updates

---
+++ precache-manifest.28c1cdde9ecca991b997274a5a2750fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6e92a0d6131d23ac187",
+    "url": "alexissdev.github.io/alexissdev.me/static/css/main.c415d96b.chunk.css"
+  },
+  {
+    "revision": "d6e92a0d6131d23ac187",
+    "url": "alexissdev.github.io/alexissdev.me/static/js/main.d6e92a0d.chunk.js"
+  },
+  {
+    "revision": "0d2fb24a2fa7d3853a0e",
+    "url": "alexissdev.github.io/alexissdev.me/static/css/1.fafe5ade.chunk.css"
+  },
+  {
+    "revision": "0d2fb24a2fa7d3853a0e",
+    "url": "alexissdev.github.io/alexissdev.me/static/js/1.0d2fb24a.chunk.js"
+  },
+  {
+    "revision": "589c66b9c69b4758d89d",
+    "url": "alexissdev.github.io/alexissdev.me/static/js/runtime~main.589c66b9.js"
+  },
+  {
+    "revision": "9763beb56a5be82adf18246cbd2723f1",
+    "url": "alexissdev.github.io/alexissdev.me/index.html"
+  }
+];
