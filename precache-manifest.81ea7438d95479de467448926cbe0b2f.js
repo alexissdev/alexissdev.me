b22ed5fe2ac0b874b9ad6935026c55df72223e7f
alexissdev <notcacha1998@gmail.com>
2023-02-15 16:47:21 -0300

    Updates

---
+++ precache-manifest.81ea7438d95479de467448926cbe0b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30455e0200b6d8769394",
+    "url": "alexissdev.github.io/alexissdev.me/static/css/main.c415d96b.chunk.css"
+  },
+  {
+    "revision": "30455e0200b6d8769394",
+    "url": "alexissdev.github.io/alexissdev.me/static/js/main.30455e02.chunk.js"
+  },
+  {
+    "revision": "0d2fb24a2fa7d3853a0e",
+    "url": "alexissdev.github.io/alexissdev.me/static/css/1.fafe5ade.chunk.css"
+  },
+  {
+    "revision": "0d2fb24a2fa7d3853a0e",
+    "url": "alexissdev.github.io/alexissdev.me/static/js/1.0d2fb24a.chunk.js"
+  },
+  {
+    "revision": "589c66b9c69b4758d89d",
+    "url": "alexissdev.github.io/alexissdev.me/static/js/runtime~main.589c66b9.js"
+  },
+  {
+    "revision": "d2f5f00142fe3bdabdca142d2ac4f311",
+    "url": "alexissdev.github.io/alexissdev.me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "alexissdev.me/precache-manifest.66b50ef5a00c5a6a9e4339fcd399ab50.js"
+  "alexissdev.github.io/alexissdev.me/precache-manifest.81ea7438d95479de467448926cbe0b2f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("alexissdev.me/index.html", {
+workbox.routing.registerNavigationRoute("alexissdev.github.io/alexissdev.me/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
