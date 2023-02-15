776fd7d100cec72a5b29e907e350f9b8c7dae70c
alexissdev <notcacha1998@gmail.com>
2023-02-15 17:00:39 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "alexissdev.github.io/alexissdev.me/precache-manifest.81ea7438d95479de467448926cbe0b2f.js"
+  "/alexissdev/alexissdev.me/precache-manifest.6a9e06e54684a4806fb910ae4db3f417.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("alexissdev.github.io/alexissdev.me/index.html", {
+workbox.routing.registerNavigationRoute("/alexissdev/alexissdev.me/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
