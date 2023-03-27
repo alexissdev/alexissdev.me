b1e119079fdeaa007044d3c54abff2e5ed2c20c6
alexissdev <notcacha1998@gmail.com>
2023-03-27 20:11:27 -0300

    feat: added the bootstrap of project

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>All rights reserved. Alexis Costa</p>
+    <footer className="text-center p-4">
+      <p className="h-4">All rights reserved. Alexis Costa</p>
     </footer>
   );
 };
