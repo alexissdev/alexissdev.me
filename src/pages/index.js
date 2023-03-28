04c5f81c107e9b56ad1507cb89748606f94b98eb
alexissdev <notcacha1998@gmail.com>
2023-03-27 21:02:10 -0300

    feat(styles): added the Navigation, ImageEffect and custom Booststrap css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer/footer";
+import Navigation from "@/components/navigation/navigation";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Navigation />
       <Footer />
     </>
   );
