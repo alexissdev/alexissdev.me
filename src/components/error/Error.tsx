ee8f9b4673fa6dea42996a3bdf40b831ab8f526d
alexissdev <notcacha1998@gmail.com>
2023-04-09 22:10:43 -0300

    refactor(error): use the Image component from Error page

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+
 export default function Error() {
   return (
     <div>
       <div className="flex flex-col vh-100 items-center gap-4 my-10 p-8">
         <br />
-        <img
+        <Image
           src={"https://http.cat/404"}
           alt="http cat"
           className="vh-90 error-img"
+          width={621}
+          height={621}
         />
       </div>
     </div>
