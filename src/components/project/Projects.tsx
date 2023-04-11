eb2ec325b75bb9dd34f8f35725ef0e068ea1c5e8
alexissdev <notcacha1998@gmail.com>
2023-04-11 16:20:58 -0300

    feat: added the Projects component from separe the component of projects

---
+++ src/components/project/Projects.tsx
@@ -0,0 +1,22 @@
+import Repository from "@/lib/github/github.repository";
+import ProjectCard from "./ProjectCard";
+
+export default function Projects({
+  repositories,
+}: {
+  repositories: Repository[];
+}) {
+  return (
+    <div>
+      <div className="container vh-100 d-flex justify-content-center align-items-center">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          {repositories.map((repository) => {
+            return (
+              <ProjectCard key={repository.name} repository={repository} />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,14 +1,12 @@
-import { useEffect } from "react";
-
 import Repository from "@/lib/github/github.repository";
 import { getRepositoryOfUser } from "@/lib/github/repository.service";
 
 import Header from "@/components/Header";
 import Navigation from "@/components/navigation/Navigation";
 import Footer from "@/components/footer/Footer";
-import ProjectCard from "@/components/project/ProjectCard";
+import Projects from "@/components/project/Projects";
 
-export default function Projects({
+export default function ProjectsPage({
   repositories,
 }: {
   repositories: Repository[];
@@ -17,15 +15,7 @@ export default function Projects({
     <>
       <Header title="Alexis | Projects" />
       <Navigation />
-      <header className="container vh-100 d-flex justify-content-center align-items-center">
-        <div className="row row-cols-1 row-cols-md-2 g-4">
-          {repositories.map((repository) => {
-            return (
-              <ProjectCard key={repository.name} repository={repository} />
-            );
-          })}
-        </div>
-      </header>
+      <Projects repositories={repositories} />
       <Footer />
     </>
   );
