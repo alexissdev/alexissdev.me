987f17485a3ec051cc13c7abe540795a4b2cbde1
alexissdev <notcacha1998@gmail.com>
2023-04-30 12:44:26 -0300

    refactor(home): change the title of home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div>
       <header className="container vh-100 d-flex justify-content-center align-items-center">
         <div className="d-flex flex-column justify-content-center align-items-center">
-          <h1 className="display-1 my-5 text-uppercase fw-bold">
-            Java Developer
-          </h1>
+          <h1 className="display-1 fw-bold">I'm Alexis</h1>
+          <h2 className="display-6 fw-bold">Full Stack Developer</h2>
+          <br />
           <HomeButtons />
         </div>
       </header>
