65d4290dbbde1e5d7b9f2da55c974f95c48989a9
alexissdev <notcacha1998@gmail.com>
2023-05-14 23:04:02 -0300

    feat: check the validate email

--- src/lib/util/contact.util.ts
+++ src/lib/util/contact.util.ts
@@ -1,11 +1,12 @@
 import Contact from "../model/contact.model";
+import { validateEmail } from "./email.util";
 
 export function checkEmptyProperties(contact: Contact): string {
   if (!contact.name) {
     return "name";
   }
 
-  if (!contact.email) {
+  if (!contact.email || !validateEmail(contact.email)) {
     return "email";
   }
 
